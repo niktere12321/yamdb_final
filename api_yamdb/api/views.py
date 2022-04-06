567c04b1e8518dfd75fbe3769654d1f7d51a7552
Никита <nikterekhoff4242@gmail.com>
2022-04-06 19:58:20 +0300

    final1.0

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,13 +7,12 @@ from django.shortcuts import get_object_or_404
 from django_filters import rest_framework as dfrf_filters
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title
+
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
 from .pagination import UserPagination
